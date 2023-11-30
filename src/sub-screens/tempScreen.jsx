6816df99dcccb145e3ae18c827adf33c90217d53
Jeffrey Fung <fung2682@connect.hku.hk>
2023-12-01 00:05:58 +0800

    update temp screen

--- src/sub-screens/tempScreen.jsx
+++ src/sub-screens/tempScreen.jsx
@@ -7,22 +7,8 @@ import { fetchAllTables } from "../API/FootApi.js";
 const TempScreen = () => {
     return (
         <View style={styles.container}>
-            <Text>temp</Text>
-            <TouchableOpacity           // test function to add data to firestore
+            <TouchableOpacity
                 onPress={() => {
-                    addData("test1", { title: "test_title" });
-                }}
-                style={{
-                    backgroundColor: "white",
-                    padding: 10,
-                    borderRadius: 10,
-                }}
-            >
-                <Text>add data</Text>
-            </TouchableOpacity>
-            <TouchableOpacity       // test function to read data from firestore
-                onPress={() => {
-                    // getData("test1", "Table");
                     fetchAllTables();
                 }}
                 style={{
@@ -31,7 +17,7 @@ const TempScreen = () => {
                     borderRadius: 10,
                 }}
             >
-                <Text>read data</Text>
+                <Text>fetch and save table(all) to firestore</Text>
             </TouchableOpacity>
         </View>
     );
