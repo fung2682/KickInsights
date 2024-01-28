341011f891c76ea79e15b2ee86074bbebb70d1fd
Jeffrey Fung <fung2682@connect.hku.hk>
2024-01-28 23:05:44 +0800

    translate FBREF club name to namecode for logo display

---
+++ src/clubNameCode.js
@@ -0,0 +1,25 @@
+// Translation from FBREF team names to KickInsights team namecodes
+// KickInsights team namecodes are used for logo display
+export const clubNameCode = {
+    // FBREF Scores and Fixtures
+    "Arsenal": "ARS",
+    "Aston Villa": "AVL",
+    "Bournemouth": "BOU",
+    "Brentford": "BRE",
+    "Brighton": "BHA",
+    "Burnley": "BUR",
+    "Chelsea": "CHE",
+    "Crystal Palace": "CRY",
+    "Everton": "EVE",
+    "Fulham": "FUL",
+    "Liverpool": "LIV",
+    "Luton Town": "LUT",
+    "Manchester City": "MCI",
+    "Manchester Utd": "MUN",
+    "Newcastle Utd": "NEW",
+    "Nott'ham Forest": "NFO",
+    "Sheffield Utd": "SHU",
+    "Tottenham": "TOT",
+    "West Ham": "WHU",
+    "Wolves": "WOL",
+}
