b4b472e59f0db02696f5969effc8bf3c0646ac6f
Jeffrey Fung <fung2682@connect.hku.hk>
2024-03-07 12:54:20 +0800

    initialized matchdetails_info screen design

--- src/sub-screens/match/MatchDetails_Info.jsx
+++ src/sub-screens/match/MatchDetails_Info.jsx
@@ -8,7 +8,7 @@ import { clubLogo } from "../../clubLogo";
 
 const MatchDetails_Info = ({matchData, matchStats}) => {
 
-  const { homeNameCode, awayNameCode, homeGoal, awayGoal } = matchData;   // immediately available
+  const { homeNameCode, awayNameCode, homeGoal, awayGoal, homeTeam, awayTeam, day, dayOfWeek, month, time, matchWeek } = matchData;
   const [home, setHome] = useState(null);
   const [away, setAway] = useState(null);
   const [general, setGeneral] = useState(null);
@@ -52,6 +52,64 @@ const MatchDetails_Info = ({matchData, matchStats}) => {
           <Image source={clubLogo[awayNameCode]} style={styles.image}/>
         </View>
 
+        <Text style={styles.infoAreaTitle}>General</Text>
+        <View style={styles.infoArea}>
+          <View style={styles.infoAreaRow}>
+            <Text style={styles.infoAreaRowKey}>Matchweek</Text>
+            <Text style={styles.infoAreaRowValue}>{matchWeek}</Text>
+          </View>
+          <View style={styles.infoAreaRow}>
+            <Text style={styles.infoAreaRowKey}>Date</Text>
+            <Text style={styles.infoAreaRowValue}>{`${day} ${month} (${dayOfWeek})`}</Text>
+          </View>
+          <View style={styles.infoAreaRow}>
+            <Text style={styles.infoAreaRowKey}>Time</Text>
+            <Text style={styles.infoAreaRowValue}>{time}</Text>
+          </View>
+          <View style={styles.infoAreaRow}>
+            <Text style={styles.infoAreaRowKey}>Attendance</Text>
+            <Text style={styles.infoAreaRowValue}>{general !== null? general.attendance : '--'}</Text>
+          </View>
+          <View style={styles.infoAreaRow}>
+            <Text style={styles.infoAreaRowKey}>Venue</Text>
+            <Text style={styles.infoAreaRowValue} numberOfLines={1} ellipsizeMode="tail">
+              {general !== null? general.venue: '--'}
+            </Text>
+          </View>
+        </View>
+
+        <Text style={styles.infoAreaTitle}>Home Team ({homeTeam})</Text>
+        <View style={styles.infoArea}>
+          <View style={styles.infoAreaRow}>
+            <Text style={styles.infoAreaRowKey}>Manager</Text>
+            <Text style={styles.infoAreaRowValue}>{home !== null? home.general.manager: '--'}</Text>
+          </View>
+          <View style={styles.infoAreaRow}>
+            <Text style={styles.infoAreaRowKey}>Captain</Text>
+            <Text style={styles.infoAreaRowValue}>{home !== null? home.general.captain: '--'}</Text>
+          </View>
+          <View style={styles.infoAreaRow}>
+            <Text style={styles.infoAreaRowKey}>Expected Goals</Text>
+            <Text style={styles.infoAreaRowValue}>{home !== null? home.general.xG: '--'}</Text>
+          </View>
+        </View>
+
+        <Text style={styles.infoAreaTitle}>Away Team ({awayTeam})</Text>
+        <View style={styles.infoArea}>
+          <View style={styles.infoAreaRow}>
+            <Text style={styles.infoAreaRowKey}>Manager</Text>
+            <Text style={styles.infoAreaRowValue}>{away !== null? away.general.manager: '--'}</Text>
+          </View>
+          <View style={styles.infoAreaRow}>
+            <Text style={styles.infoAreaRowKey}>Captain</Text>
+            <Text style={styles.infoAreaRowValue}>{away !== null? away.general.captain: '--'}</Text>
+          </View>
+          <View style={styles.infoAreaRow}>
+            <Text style={styles.infoAreaRowKey}>Expected Goals</Text>
+            <Text style={styles.infoAreaRowValue}>{away !== null? away.general.xG: '--'}</Text>
+          </View>
+        </View>
+        
       </View>
 
       <View style={styles.tableContainer}>
@@ -64,6 +122,33 @@ const MatchDetails_Info = ({matchData, matchStats}) => {
           </>
         }
       </View>
+
+      <View style={styles.container}>
+        <Text style={styles.infoAreaTitle}>Officials</Text>
+        <View style={styles.infoArea}>
+          <View style={styles.infoAreaRow}>
+            <Text style={styles.infoAreaRowKey}>Referee</Text>
+            <Text style={styles.infoAreaRowValue}>{general !== null? general.referee: '--'}</Text>
+          </View>
+          <View style={styles.infoAreaRow}>
+            <Text style={styles.infoAreaRowKey}>Assistant Ref. 1</Text>
+            <Text style={styles.infoAreaRowValue}>{general !== null? general.assistantReferee1: '--'}</Text>
+          </View>
+          <View style={styles.infoAreaRow}>
+            <Text style={styles.infoAreaRowKey}>Assistant Ref. 2</Text>
+            <Text style={styles.infoAreaRowValue}>{general !== null? general.assistantReferee2: '--'}</Text>
+          </View>
+          <View style={styles.infoAreaRow}>
+            <Text style={styles.infoAreaRowKey}>Fourth Official</Text>
+            <Text style={styles.infoAreaRowValue}>{general !== null? general.fourthOfficial: '--'}</Text>
+          </View>
+          <View style={styles.infoAreaRow}>
+            <Text style={styles.infoAreaRowKey}>VAR Referee</Text>
+            <Text style={styles.infoAreaRowValue}>{general !== null? general.varReferee: '--'}</Text>
+          </View>
+        </View>
+
+      </View>
     </ScrollView>
   );
 }
@@ -75,6 +160,7 @@ const styles = StyleSheet.create({
     flexDirection: "column",
   },
   container: {
+    backgroundColor: "#1f1f1f",
     alignSelf: "center",
     width: "97%",
     height: "fit-content",
@@ -121,7 +207,43 @@ const styles = StyleSheet.create({
   tableContainer: {
     width: "97%",
     alignSelf: "center",
-  }
+  },
+  infoArea: {
+    flexDirection: "column",
+    justifyContent: "flex-start",
+    alignItems: "center",
+    height: 'fit-content',
+    marginBottom: 5,
+	},
+  infoAreaTitle: {
+		color: "white",
+    textAlign: "left",
+		fontSize: 15,
+		fontWeight: "bold",
+    paddingLeft: 10,
+		marginTop: 8,
+		marginBottom: 2,
+	},
+  infoAreaRow: {
+		flexDirection: "row",
+		justifyContent: "flex-start",
+		alignItems: "center",
+		width: "100%",
+		marginBottom: 1,
+	},
+  infoAreaRowKey: {
+		width: "35%",
+		textAlign: "left",
+		color: "white",
+		fontSize: 15,
+    paddingLeft: 10,
+	},
+  infoAreaRowValue: {
+		width: "65%",
+		textAlign: "center",
+		color: "white",
+		fontSize: 15,
+	},
 });
 
 export default MatchDetails_Info;
