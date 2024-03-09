c66d6063dfe929ccd9612d94c4d08709ef1e4b7a
Jeffrey Fung <fung2682@connect.hku.hk>
2024-03-09 19:07:06 +0800

    added predict button for future or suspended matches

--- src/sub-screens/match/MatchDetails.jsx
+++ src/sub-screens/match/MatchDetails.jsx
@@ -1,10 +1,11 @@
 import React, {useState, useEffect} from "react";
-import { Text, View, StyleSheet } from "react-native";
+import { Text, View, StyleSheet, TouchableOpacity } from "react-native";
 import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
 import MatchDetails_Info from "./MatchDetails_Info";
 import MatchDetails_Stats from "./MatchDetails_Stats";
 import MatchDetails_Squad from "./MatchDetails_Squad";
 import { getData } from "../../firebase/firestore";
+import { useNavigation } from "@react-navigation/native";
 
 const Tab = createMaterialTopTabNavigator();
 
@@ -12,11 +13,27 @@ const MatchDetails = ({props}) => {
 
     const matchData = props.route.params.match;
     const [matchStats, setMatchStats] = useState(null);
+    const nav = useNavigation();
 
     useEffect(() => {
         const fetchMatchStats = async() => {
-            const matchStats = await getData("matches", props.route.params.match.matchRef);
-            setMatchStats(matchStats);
+            if ((matchData.matchRef === "NA") || (matchData.matchRef === "Match Postponed")) {  // the game is postponed or in future
+                setMatchStats("NA");
+                nav.setOptions({ 
+                    headerRight: () => (
+                        <TouchableOpacity 
+                            style={styles.predictButton}
+                            activeOpacity={0.8}
+                            onPress={() => nav.navigate("COMMUNITY")}   // temporary
+                        >
+                            <Text style={styles.predictButtonText}>Predict</Text>
+                        </TouchableOpacity>
+                    ),
+                });
+            } else {
+                const matchStats = await getData("matches", matchData.matchRef);
+                setMatchStats(matchStats);
+            }
         }
         fetchMatchStats();
     }, []);
@@ -59,5 +76,22 @@ const MatchDetails = ({props}) => {
     );
 }
 
+const styles = StyleSheet.create({
+    predictButton: {
+        backgroundColor: "#54a761",
+        padding: 5,
+        borderRadius: 5,
+        marginRight: 5,
+        width: 70,
+        height: 28,
+        alignItems: "center",
+        justifyContent: "center",
+    },
+    predictButtonText: {
+        color: "white",
+        fontWeight: "bold",
+        fontSize: 15,
+    },
+});
 
 export default MatchDetails;
