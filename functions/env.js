e125f5efee6367e1226beb8f3df0b0f1104f975f
Jeffrey Fung <fung2682@connect.hku.hk>
2024-03-30 13:10:59 +0800

    cloud functions set up

---
+++ functions/env.js
@@ -0,0 +1,15 @@
+// credentials for firebase
+const apiKey="AIzaSyA2sHvoHBLu9ZiyxQzbU8C95VNRTA4NOE0"
+const authDomain="kickinsights-ccc1e.firebaseapp.com"
+const projectId="kickinsights-ccc1e"
+//storageBucket="kickinsights-ccc1e.appspot.com"
+const messagingSenderId="489611157233"
+const appId="1:489611157233:web:f64d2707fcdb5f53db9350"
+const measurementId="G-M3EX1V1R97"
+const storageBucket="gs://kickinsights-ccc1e.appspot.com"
+
+// credentials for FootApi
+const X_RapidAPI_Key='2d38569861mshff35f4f34de88e4p1ff015jsn6729c1c3f3d0'
+const X_RapidAPI_Host='footapi7.p.rapidapi.com'
+
+export { apiKey, authDomain, projectId, storageBucket, messagingSenderId, appId, measurementId, X_RapidAPI_Key, X_RapidAPI_Host }
