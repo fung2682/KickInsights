6e32a161872d55bc45bb10021ebe5b7c0612ec4a
Jeffrey Fung <fung2682@connect.hku.hk>
2024-03-31 19:06:30 +0800

    updated tempscreem (migrate all fetch scripts to cloud)

--- src/sub-screens/tempScreen.jsx
+++ src/sub-screens/tempScreen.jsx
@@ -11,71 +11,66 @@ import fetchCloud from "../fetchCloud.jsx";
 
 const TempScreen = () => {
 
-    return (
-        <View style={styles.container}>
-            <TouchableOpacity
-                onPress={() => {fetchTables()}} style={styles.temp_button}
-            >
-                <Text>{`[league tables] (hourly?)\n (3) FootApi -> Firestore`}</Text>
-            </TouchableOpacity>
-            <TouchableOpacity
-                onPress={ async () => {
-                    await fetchClubs();
-                    await fetchCloud(4);
-                    await fetchPlayerImage();
-                    await fetchClubs();
-                }} 
-                style={styles.temp_button}
-            >
-                <Text>{`[club details] (no need)`}</Text>
-                <Text>{`(10 mins, 40) FootApi + Storage -> Firestore`}</Text>
-                <Text>{`[player images] (only fetch new ones)\n (~1 min) FootApi -> Firebase Storage`}</Text>
-                <Text>{`1. get all players\n2.download all players' images to storage`}</Text>
-                <Text>{`3.update new players with images`}</Text>
-            </TouchableOpacity>
-            {/* <TouchableOpacity
-                onPress={() => {fetchPlayerImage()}} style={styles.temp_button}
-            >
-                <Text>{`[player images] (only fetch new ones)\n (~1 min) FootApi -> Firebase Storage`}</Text>
-            </TouchableOpacity> */}
-            <TouchableOpacity
-                onPress={() => {fetchLastNext3()}} style={styles.temp_button}
-            >
-                <Text>{`[lastNext 3] (hourly?)\n (40) (hourly?) FootApi -> Firestore`}</Text>
-            </TouchableOpacity>
-            <TouchableOpacity
-                onPress={() => {fetchClubStats()}} style={styles.temp_button}
-            >
-                <Text>{`[club stats] (daily?)\n (15 mins) FBREF -> Firestore`}</Text>
-            </TouchableOpacity>
-            <TouchableOpacity
-                onPress={() => {fetchPlayers()}} style={styles.temp_button}
-            >
-                <Text>{`[player list + stats] (hourly?)\n (10 mins, 40) FootApi + Storage -> Firestore`}</Text>
-            </TouchableOpacity>
-            <TouchableOpacity
-                onPress={() => {fetchMatches()}} style={styles.temp_button}
-            >
-                <Text>{`[match info + stats] (daily?) (20 mins) FBREF -> Firestore`}</Text>
-            </TouchableOpacity>
-        </View>
-    );
+	return (
+		<View style={styles.container}>
+			{/* <TouchableOpacity
+				onPress={() => { fetchTables() }} style={styles.temp_button}
+			>
+				<Text>{`[league tables] (hourly?)\n (3) FootApi -> Firestore`}</Text>
+			</TouchableOpacity> */}
+			{/* <TouchableOpacity
+				onPress={async () => {
+					await fetchClubs();
+					await fetchCloud(4);
+					await fetchPlayerImage();
+					await fetchClubs();
+				}}
+				style={styles.temp_button}
+			>
+				<Text>{`[club details] (no need)`}</Text>
+				<Text>{`(10 mins, 40) FootApi + Storage -> Firestore`}</Text>
+				<Text>{`[player images] (only fetch new ones)\n (~1 min) FootApi -> Firebase Storage`}</Text>
+				<Text>{`1. get all players\n2.download all players' images to storage`}</Text>
+				<Text>{`3.update new players with images`}</Text>
+			</TouchableOpacity> */}
+			{/* <TouchableOpacity
+				onPress={() => { fetchLastNext3() }} style={styles.temp_button}
+			>
+				<Text>{`[lastNext 3] (hourly?)\n (40) (hourly?) FootApi -> Firestore`}</Text>
+			</TouchableOpacity> */}
+			{/* <TouchableOpacity
+				onPress={() => { fetchClubStats() }} style={styles.temp_button}
+			>
+				<Text>{`[club stats] (daily?)\n (30 mins) FBREF -> Firestore`}</Text>
+			</TouchableOpacity> */}
+			{/* <TouchableOpacity
+				onPress={() => { fetchPlayers() }} style={styles.temp_button}
+			>
+				<Text>{`[player list + stats] (hourly?)\n (10 mins, 40) FootApi + Storage -> Firestore`}</Text>
+			</TouchableOpacity> */}
+			{/* <TouchableOpacity
+				onPress={() => { fetchMatches() }} style={styles.temp_button}
+			>
+				<Text>{`[match info + stats] (daily?) (20 mins) FBREF -> Firestore`}</Text>
+			</TouchableOpacity> */}
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: "#3a3a3a",
-        alignItems: "center",
-        justifyContent: "center",
-    },
-    temp_button: {
-        backgroundColor: "white",
-        padding: 10,
-        borderRadius: 5,
-        width: "80%",
-        marginBottom: 10,
-    }
+	container: {
+		flex: 1,
+		backgroundColor: "#3a3a3a",
+		alignItems: "center",
+		justifyContent: "center",
+	},
+	temp_button: {
+		backgroundColor: "white",
+		padding: 10,
+		borderRadius: 5,
+		width: "80%",
+		marginBottom: 10,
+	}
 });
 
 export default TempScreen;
